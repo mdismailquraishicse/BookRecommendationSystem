fbc4645835ead97d9b755e286b21f6658cf0f44f
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2024-05-07 09:50:18 +0530

    remove_punctuation_and_integer function defined.

--- my_module.py
+++ my_module.py
@@ -1,5 +1,21 @@
+# This function displays basic details of the dataset
 def knowYourData(df):
+  ''' Enter a dataframe '''
   print('Shape: ', df.shape)
   print('Duplicates count: ', df.duplicated().sum())
   print('Columns:')
   print(list(df.columns))
+# This function removes punctuations and integers from a given sentence.
+def remove_punctuations_and_integers(text):
+    ''' Enter texts '''
+    punc= list(string.punctuation)
+    new_text= []
+    result = ''
+    for t in text.split():
+        if t.isnumeric()==False:
+            new_text.append(t)
+    new_text= ' '.join(new_text)
+    for i in new_text:
+        if i not in punc:
+            result += i
+    return result
