4a1223fbe950da3b9299720cb6552b4ed5467f40
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2024-05-09 17:07:44 +0530

    very simple project done completely in working condition

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
 # Libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import pickle
 import pandas as pd
+import my_module
 
 app= Flask(__name__) # flask object creation
 
-# Loading pickle files
-book_index = list(pickle.load(open('similarity_columns.pkl','rb')))
-similarity_scores= dict(pickle.load(open('similarity_score.pkl','rb')))
 dict_needed= dict(pickle.load(open('dict_required.pkl','rb')))
-# Making dataframes
 dict_needed= pd.DataFrame(dict_needed)
-similarity_df = pd.DataFrame(similarity_scores,columns=book_index)
-# Recommend function
-def recommend(book_title):
-    recommended_titles = similarity_df[book_title].sort_values(ascending=False).reset_index()
-    recommended_titles = recommended_titles.rename(columns={book_title:'similarity_score'}).head(6)
-    full_data = dict_needed[dict_needed['Book-Title'].isin(recommended_titles['Book-Title'])]
-    full_data = full_data.merge(recommended_titles, on='Book-Title')
-    recommendations = full_data.sort_values(by='similarity_score', ascending=False).iloc[1:6]
-    return recommendations
+
 # Function for index page
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           titles=dict_needed['Book-Title'])
 
 # Function for recommendation page rendering
-@app.route('/recommendation')
+@app.route('/recommendation',methods=['post'])
 def recommendation():
-    results = recommend("The Dead Zone")
+    user_input= request.form.get('user_input')
+    results= my_module.recommend(user_input)
     return render_template('recommend.html',
-                           titles= results['Book-Title'],
-                           image= results['Image-URL-M'],
-                           publisher = results['Publisher'],
-                           yop = results['Year-Of-Publication'],
-                           author = results['Book-Author']
+                           titles= results['Book-Title'].values,
+                           image= results['Image-URL-M'].values,
+                           publisher = results['Publisher'].values,
+                           yop = results['Year-Of-Publication'].values,
+                           author = results['Book-Author'].values
                            )
 
 
--- my_module.py
+++ my_module.py
@@ -1,5 +1,17 @@
 from nltk.stem import SnowballStemmer
 import string
+import pickle
+import pandas as pd
+import numpy as np
+
+# Loading pickle files
+book_index = list(pickle.load(open('similarity_columns.pkl','rb')))
+similarity_scores= dict(pickle.load(open('similarity_score.pkl','rb')))
+dict_needed= dict(pickle.load(open('dict_required.pkl','rb')))
+# Making dataframes
+dict_needed= pd.DataFrame(dict_needed)
+similarity_df = pd.DataFrame(similarity_scores,columns=book_index)
+
 # This function displays basic details of the dataset
 def knowYourData(df):
   ''' Enter a dataframe '''
@@ -32,3 +44,11 @@ def stemming(text):
     for t in text.split():
         result.append(ss.stem(t))
     return ' '.join(result)
+# Recommend function
+def recommend(book_title):
+    recommended_titles = similarity_df[book_title].sort_values(ascending=False).reset_index()
+    recommended_titles = recommended_titles.rename(columns={book_title:'similarity_score'}).head(6)
+    full_data = dict_needed[dict_needed['Book-Title'].isin(recommended_titles['Book-Title'])]
+    full_data = full_data.merge(recommended_titles, on='Book-Title')
+    recommendations = full_data.sort_values(by='similarity_score', ascending=False).iloc[1:6]
+    return recommendations
